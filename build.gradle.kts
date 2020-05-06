918aa8b0d6a056028da4d82cc45cd1a2ca740575
Duncan Calvert <dcalvert@ihmc.us>
2020-05-05 22:59:04 -0500

    :arrow_up: ihmc-commons 0.29.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,11 @@ mainDependencies {
    api("org.jfree:jcommon:1.0.21")
 
    api("us.ihmc:euclid:0.14.0")
-   api("us.ihmc:ihmc-commons:0.28.2")
+   api("us.ihmc:ihmc-commons:0.29.0")
    api("us.ihmc:ihmc-graphics-description:0.16.0")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.28.2")
+   api("us.ihmc:ihmc-commons-testing:0.29.0")
    api("us.ihmc:ihmc-graphics-description-test:0.16.0")
 }
