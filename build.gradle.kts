e6a49e402d747dd634c0b64e9ac8cb115e2c74c8
Sylvain Bertrand <sbertrand@ihmc.us>
2020-06-18 09:21:59 -0500

    :arrow_up: euclid 0.15.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ mainDependencies {
    api("org.jfree:jfreechart:1.0.17")
    api("org.jfree:jcommon:1.0.21")
 
-   api("us.ihmc:euclid:0.14.2-ejml-0.39-beta-1")
+   api("us.ihmc:euclid:0.15.0")
    api("us.ihmc:ihmc-commons:0.29.0")
    api("us.ihmc:ihmc-graphics-description:0.17.0-ejml-0.39-beta-1")
 }
