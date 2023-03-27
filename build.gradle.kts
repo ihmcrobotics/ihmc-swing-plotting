d7694a347d5c50fee8443a99a98a8b184239353f
Sylvain Bertrand <sbertrand@ihmc.org>
2023-03-27 15:07:05 -0500

    :bookmark: 0.19.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.19.10"
+   version = "0.19.11"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-swing-plotting"
    openSource = true
    
